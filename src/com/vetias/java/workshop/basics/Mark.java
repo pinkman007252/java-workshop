8cd0c094746c2412c1f8b0fe264f1aa4a8c415fc
pinkman007252 <zameeruddin1613@gmail.com>
2025-07-02 15:34:32 +0530

    code of code

---
+++ src/com/vetias/java/workshop/basics/Mark.java
@@ -0,0 +1,10 @@
+public class Mark {
+    public static void main(String[] args){
+        int[]mark=(20,30,40,50,60,70);
+        for(int mark:marks){
+            System.out.println(mark);
+
+
+    }
+}
+}
