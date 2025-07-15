3b228eab8eda62dd8d79b18ec7331730d231f701
pinkman007252 <zameeruddin1613@gmail.com>
2025-07-15 09:51:38 +0530

    update to git

--- src/main/java/com/vetias/java/workshop/basics/tem/bin/Dao/OrganaizationDao.java
+++ src/main/java/com/vetias/java/workshop/basics/tem/bin/Dao/OrganaizationDao.java
@@ -1,37 +1,26 @@
 package com.vetias.java.workshop.basics.tem.bin.Dao;
-
-
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class OrganizationDao {
+public class Organizationdao{
 
-    public void createTable (){
-        try {
-            Class.forName("org.h2.Driver");
-        } catch (ClassNotFoundException e) {
-             e.printStackTrace();
-        }
-      
-        try(Connection connection = DriverManager.getConnection("jdbc:h2:mem:tempdao","sa",null);
-            Statement statement = connection.createStatement()) {
-            statement.execute("""
-                    Create Table Organization(
-                    id int AUTO_INCREMENT PRIMARY KEY,
+    public void createTable(Connection dbConnection) {
+        try (Statement Statement = dbConnection.createStatement()) {
+      Statement.execute ( """
+                CREATE TABLE Organization (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
                     NAME VARCHAR(100),
                     WEBSITE VARCHAR(100),
                     EMAIL VARCHAR(100),
                     CONTACT_NUMBER VARCHAR(100),
                     REGISTRATION_NO INT,
-                    ADDRESS VARCHAR(255))
-                    """);
-        } catch (SQLException sqlException) {
-            System.out.println("Error creating table:"+ sqlException );
-        }
+                    ADDRESS VARCHAR(255)
+                )
+                """);
 
+        } catch (SQLException sqlException) {
+            System.out.println("Error creating table: " + sqlException);
+        }
     }
 }
-
-
